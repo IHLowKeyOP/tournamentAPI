1331addc5e0646c1308eb5a87f04bcacb049d4d7
Chad Barcelo <chadbarcelo@outlook.com>
2018-08-01 10:42:49 -0400

    made the  tournament model

---
+++ models/tournament.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose'); 
+const Schema = mongoose.Schema        
+
+const tournamentSchema = new Schema({
+    administrator: {type: Schema.Types.ObjectId, ref: "User"},
+    teams: [{type: Schema.Types.ObjectId, ref: "Team"}],
+    winnerCondition: Boolean,
+    winner: [{type: Schema.Types.ObjectId, ref: "Team"}],
+    tournamentType: {type: String, default: "insert tournament type"},
+    rules: {type: String, default: "insert tournament rules"}
+    },
+    { timestamps: true }
+);
+
+const Tournament = mongoose.model("Tournament", tournamentSchema);
+
+module.exports = Tournament;
